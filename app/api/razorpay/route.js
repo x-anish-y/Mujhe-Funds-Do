4ac19aa89ed1f6fd41c1e6947f048cfd45a0dafb
x-anish-y <anishsasmal5481@gmail.com>
2025-12-25 22:52:34 +0530

    Create route.js

---
+++ app/api/razorpay/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { validatePaymentVerification } from "razorpay/dist/utils/razorpay-utils";
+import Payment from "@/app/models/Payment";
+import Razorpay from "razorpay";
+import connectDB from "@/app/db/connectdb";
+
+export const POST = async (request) => {
+    await connectDB();
+    let body = await request.formData();
+    body = Object.fromEntries(body);
+
+    // Check if razorpay orderid is present on the server
+    let p = await Payment.findOne({ oid: body.razorpay_order_id });
+    if(!p){
+        return NextResponse.json({success: false, message: "Order not found"}, {status: 400});
+    }
+
+    let xx = validatePaymentVerification({ "order_id": body.razorpay_order_id, "payment_id": body.razorpay_payment_id}, body.razorpay_signature, process.env.KEY_SECRET);
+
+    if (xx) {
+        const updatedPayment = await Payment.findOneAndUpdate({oid: body.razorpay_order_id}, { done: true }, { new: true });
+        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/${updatedPayment.to_username}?paymentSuccess=true`);
+    }
+    else{
+        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/${p.to_username}?paymentSuccess=false`);
+    }
+    }
